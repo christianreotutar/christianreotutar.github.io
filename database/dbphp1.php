85f3c5a163a76fb7e8866558306dbda821174bc8
Christian <christian.reotutar@yahoo.com>
2014-11-06 12:28:22 -0500

    Database Project

---
+++ database/dbphp1.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        Test
+        <?php
+            /*include 'conf.php';
+            include 'open.php'; */
+
+            $n1 = $_POST["n1"];
+            $n2 = $_POST["n2"];
+
+            echo "This is n1: " . $n1;
+        ?>
+    </body>
+
+</html>
