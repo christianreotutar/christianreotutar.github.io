b84aabec975e7ba04cc2f5af9222163c99241142
Christian <christian.reotutar@yahoo.com>
2014-12-13 02:08:34 -0500

    Google Analytics

--- physicsformulas/physics.js
+++ physicsformulas/physics.js
@@ -1,9 +1,22 @@
 $(document).ready( function() {
 
+    /* GOOGLE ANALYTICS */
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-57622356-1', 'auto');
+    ga('send', 'pageview');
+    /* GOOGLE ANALYTICS */
+
+    /* MATHJAX */
     MathJax.Hub.Queue(
         [test]
     );
+    /* MATHJAX */
 
+    /* MASONRY */
     var $container = $('#container');
     $container.masonry({
         columnWidth: 8,
@@ -14,6 +27,7 @@ $(document).ready( function() {
     function test() {
         masonry.layout();
     }
+    /* MASONRY */
 
     jQuery.fn.toggle = function( fn, fn2 ) {
         // Don't mess with animation or css toggles
@@ -83,6 +97,21 @@ $(document).ready( function() {
         function() {
             $('#mid3').css("background-color", "#93DB70");
             $midterm3.show();
+            masonry.layout();
+        }
+    );
+
+    var $finals = $('.posthalf.final');
+    $('#final').toggle(
+        function() {
+            $('#final').css("background-color", "transparent");
+            $finals.hide();
+            masonry.layout();
+        },
+
+        function() {
+            $('#final').css("background-color", "#DBC4B2");
+            $finals.show();
             masonry.layout();
         }
     );
